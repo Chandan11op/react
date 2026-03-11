4a79b28d0f693ee668e65aa6bb9e6278f16418e6
Chandan Tiwadi <chandan110906@gmail.com>
2026-03-11 19:23:09 +0530

    Commiting the Basic learnings in react and Vite+react

---
+++ basicreact/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <br/>
+      <h2>Hello MySelf Chandan </h2>
+    </>
+  );
+}
+
+export default App;
---
+++ vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World </h1>
+      <br></br>
+      <h2>My Self Chandan Using Vite</h2>
+    </>
+  )
+}
+
+export default App
