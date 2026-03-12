1badaa289ecf16d03463c7f6cdc3dfcc62e26bdd
Chandan Tiwadi <chandan110906@gmail.com>
2026-03-12 10:26:55 +0530

    Custom React Folder Practice

---
+++ customreact/customreact.js
@@ -0,0 +1,25 @@
+function customRender(reactElement, container){
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in object){
+        if (object.hasOwnProperty.call(object, prop)){
+            const element = object[prop];
+        }
+    }
+}
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click Me to Visit Google'
+}
+const mainContainer = document.querySelector('#root')
+customRender(reactElement, mainContainer)
+
+// const domElement = document.createElement(reactElement.type)
+// domElement.innerHTML = reactElement.children
+// domElement.setAttribute('href', reactElement.props.href)
+// domElement.setAttribute('target', reactElement.props.target)
+// container.appendChild(domElement)
